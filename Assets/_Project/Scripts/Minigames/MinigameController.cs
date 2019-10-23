888c4f25cf362218cab4b332fcaeb5fed997afe8
Evans Chen <evansschen@gmail.com>
2019-10-23 12:32:55 -0500

    Adds minigame support
    Can now create basic minigames

---
+++ Assets/_Project/Scripts/Minigames/MinigameController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/**
+ * Randomly chooses minigames, and creates minigame prefabs when necessary.
+ * When a minigame completes, spawns a transition and creates the next minigame.
+ * Minigames, when finished, report to this object via FinishMinigame()
+ * 
+ * MinigameController also has a timer, since each Minigame must complete in 5 seconds.
+ */
+public class MinigameController : MonoBehaviour
+{
+    // values specified by the Unity Editor
+    public GameObject tunnelPrefab;
+    public GameObject transitionPrefab;
+    public GameObject notificationPrefab;
+    public List<GameObject> minigamePrefabs;
+
+    List<GameObject> minigamesRandomlyOrdered;
+    int index = 0;
+    GameObject prevMinigame;
+    public GameObject minigame;
+    GameObject transition;
+    Tunnel tunnel;
+
+    bool finish;
+    bool ready = true;
+    bool minigameSuccess = false;
+
+    // minigames have a time cap
+    Minigame minigameState;
+    float timer = 0f;
+    public float maxTime = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minigameState = minigame.GetComponentInChildren<Minigame>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((finish || timer > maxTime) && ready)
+        {
+            ready = false;
+            finish = false;
+            // play all minigames before repeating one
+            // TODO: allow for minigames with different rotation
+            index++;
+            if (index >= minigamePrefabs.Count)
+            {
+                ShuffleMinigames();
+                index = 0;
+            }
+            // start transition
+            StartCoroutine(TransitionMinigame());
+        }
+        if (minigameState != null && minigameState.GetActive())
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    // called by Minigame component to signal that a minigame has finished
+    // triggers the transition to the next minigame
+    public void FinishMinigame()
+    {
+        finish = true;
+        Minigame m = minigame.GetComponentInChildren<Minigame>();
+        if (m == null)
+        {
+            throw new MissingComponentException("GameObject" + minigame + " has no child with a Minigame component.");
+        }
+        minigameSuccess = m.success;
+    }
+    IEnumerator TransitionMinigame()
+    {
+        // disable previous minigame
+        minigame.GetComponentInChildren<Minigame>().Disable();
+        timer = 0f;
+        // display a notification that the player succeeded/failed
+        string message = "Failure!";
+        if (minigameSuccess)
+        {
+            message = "Success!";
+        }
+        Instantiate(notificationPrefab).GetComponentInChildren<UINotification>().Initialize(message);
+        yield return new WaitForSeconds(1f);
+        // create next minigame
+        CreateNextMinigame();
+        while (transition != null)
+        {
+            yield return null;
+        }
+        // create 
+        minigameState = minigame.GetComponentInChildren<Minigame>();
+        if (minigameState == null)
+        {
+            throw new MissingComponentException("GameObject" + minigame + " has no child with a Minigame component.");
+        }
+        Instantiate(notificationPrefab).GetComponentInChildren<UINotification>().Initialize(minigameState.title);
+        DestroyPreviousMinigame();
+        // activate next minigame
+        yield return new WaitForSeconds(1f);
+        minigameState.Enable();
+    }
+
+    // create the game objects for the next minigame, and pass them to the transition object
+    private void CreateNextMinigame()
+    {
+        SpriteRenderer prevMinigameBg = GetMinigameBackground(minigame);
+        // create tunnel
+        tunnel = Instantiate(tunnelPrefab).GetComponent<Tunnel>();
+        Vector3 tunnelPos = tunnel.SetPosition(prevMinigameBg);
+        tunnel.SetTargetAlpha(1f);
+        // create next minigame (align left bottom corner with right bottom corner of tunnel)
+        prevMinigame = minigame;
+        minigame = Instantiate(minigamePrefabs[index]);
+        SpriteRenderer minigameBg = GetMinigameBackground(minigame);
+        Vector3 bgPos = minigame.transform.position - minigameBg.transform.position;
+        Vector3 pos = tunnelPos + minigameBg.bounds.extents + bgPos;
+        minigame.transform.position = new Vector3(pos.x, pos.y, minigame.transform.position.z);
+        // kick off transition object
+        transition = Instantiate(transitionPrefab);
+        transition.GetComponent<MinigameTransition>().Initialize(prevMinigameBg, tunnel.gameObject, minigameBg);
+    }
+    // run once the transition object completes
+    private void DestroyPreviousMinigame()
+    {
+        Destroy(prevMinigame);
+        StartCoroutine(DestroyTunnel());
+    }
+
+    private SpriteRenderer GetMinigameBackground(GameObject minigame)
+    {
+        Transform[] children = minigame.GetComponentsInChildren<Transform>();
+        foreach (Transform child in children)
+        {
+            // look for minigame background
+            if (child.tag == "MinigameBackground")
+            {
+                SpriteRenderer bg = child.GetComponent<SpriteRenderer>();
+                if (bg == null)
+                {
+                    throw new ArgumentException("Minigame GameObject " + gameObject + "'s MinigameBackground child has no SpriteRenderer.");
+                }
+                return bg;
+            }
+        }
+        throw new ArgumentException("Minigame GameObject " + gameObject + " has no child with a MinigameBackground tag.");
+    }
+
+    // shuffle minigame order
+    // TODO: allow for minigames with different rotation
+    private void ShuffleMinigames()
+    {
+        System.Random r = new System.Random();
+        minigamesRandomlyOrdered = Enumerable.Range(0, minigamePrefabs.Count).OrderBy(x => r.Next()).Select(i => minigamePrefabs[i]).ToList();
+    }
+
+    // destroy the tunnel graphic at end of transition
+    IEnumerator DestroyTunnel()
+    {
+        if (tunnel != null)
+        {
+            tunnel.SetTargetAlpha(0f);
+        }
+        while (tunnel != null && tunnel.alpha != 0f)
+        {
+            yield return null;
+        }
+        if (tunnel != null)
+        {
+            Destroy(tunnel.gameObject);
+        }
+        ready = true;
+    }
+
+    public float GetTimer()
+    {
+        return 1f - (timer / maxTime);
+    }
+}
---
+++ Assets/_Project/Scripts/Transitions/MinigameTransition.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Transition animation between minigames, created by the MinigameController.
+ * Uses a sigmoid curve to move the camera.
+ * Notifies the MinigameController that the transition is complete using Destroy(gameObject)
+ * 
+ * TODO: Rotation transitions, where the screen rotates and the player falls
+ */
+public class MinigameTransition : MonoBehaviour
+{
+    Vector2 prevPos;
+    Vector2 targetPos;
+
+    float prevSize;
+    float centerSize;
+    float targetSize;
+
+    float time = 0f;
+    float duration = 1.5f;
+
+    // if true, make a beeline straight for the goal
+    // activated when keyframes finish
+    // when we reach the goal, we are done
+    bool finish;
+
+    SpriteRenderer prevMinigame;
+    GameObject tunnel;
+    SpriteRenderer nextMinigame;
+    public void Initialize(SpriteRenderer prevMinigame, GameObject tunnel, SpriteRenderer nextMinigame)
+    {
+        this.prevMinigame = prevMinigame;
+        this.tunnel = tunnel;
+        this.nextMinigame = nextMinigame;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevPos = Camera.main.transform.position;
+        targetPos = nextMinigame.transform.position;
+        prevSize = Camera.main.orthographicSize;
+        targetSize = GetOrthographicSize(nextMinigame);
+        centerSize = 1.5f * 0.5f * (prevSize + targetSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        // if we finish animation, make a beeline for target
+        if (time > duration)
+        {
+            Vector2 pos = Vector2.MoveTowards(Camera.main.transform.position, targetPos, 5f * Time.deltaTime);
+            Camera.main.transform.position = new Vector3(pos.x, pos.y, Camera.main.transform.position.z);
+            Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetSize, 5f * Time.deltaTime);
+            if ((Vector2)Camera.main.transform.position == targetPos && Camera.main.orthographicSize == targetSize)
+            {
+                Destroy(gameObject);
+            }
+            return;
+        }
+        // sigmoid curve to target
+        Vector2 cameraPos = Vector2.Lerp(prevPos, targetPos, Sigmoid(time / duration, -0.3f));
+        Camera.main.transform.position = new Vector3(cameraPos.x, cameraPos.y, Camera.main.transform.position.z);
+        float size = Mathf.Lerp(prevSize, centerSize, Sigmoid(2 * time / duration, -0.3f));
+        if (2 * time > duration)
+        {
+            size = Mathf.Lerp(centerSize, targetSize, Sigmoid((2 * time - duration) / duration, -0.3f));
+        }
+        Camera.main.orthographicSize = size;
+    }
+
+    private float GetOrthographicSize(SpriteRenderer bg)
+    {
+        float aspectRatio = (float)Screen.width / Screen.height;
+        return Mathf.Max(bg.bounds.extents.x / aspectRatio, bg.bounds.extents.y);
+    }
+
+    // maps [0, 1] to [0, 1] using a sigmoid curve.
+    // k = 0 means linear map, otherwise means intensity of velocity smoothing.
+    private float Sigmoid(float x, float k)
+    {
+        float t = Mathf.Lerp(-1f, 1f, x);
+        float s = (t - (t * k)) / (k - Mathf.Abs(t) * 2f * k + 1f);
+        return Mathf.InverseLerp(-1f, 1f, s);
+    }
+}
