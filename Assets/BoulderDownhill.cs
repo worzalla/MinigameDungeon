b2ea83b95cb4e8d55fcd6458c00bda921066bdc5
vmanoharan <36799039+vmanoharan@users.noreply.github.com>
2019-10-28 20:29:59 -0500

    Log Jump
    Not working but stuffs there

---
+++ Assets/BoulderDownhill.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderDownhill : MonoBehaviour
+{
+    public GameObject Boulder;
+    protected Rigidbody2D body;
+    public float slowdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            body.velocity = body.velocity + new Vector2((float)-1*slowdown, (float)slowdown);
+        }
+        else
+        {
+            //body.velocity = body.velocity + new Vector2((float)0.05, (float)-0.05);
+
+        }
+    }
+}
---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public GameObject pikachu;
+    protected Rigidbody2D body;
+    public int force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && body.velocity == new Vector2(0,0))
+        {
+            body.AddForce(new Vector2(0, 2*force));
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Boulder")
+        {
+            //Set fail
+        }
+    }
+}
