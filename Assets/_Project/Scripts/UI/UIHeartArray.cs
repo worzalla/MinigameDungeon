86ddeaf1825757a7ecb7c8e94cb1c2155c4c7a24
Evans Chen <evansschen@gmail.com>
2019-11-20 13:53:04 -0600

    Adds health display animation

---
+++ Assets/_Project/Scripts/UI/UIHeartArray.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHeartArray : MonoBehaviour
+{
+    List<Animator> hearts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hearts = new List<Animator>() {
+            transform.Find("heart0").gameObject.GetComponent<Animator>(),
+            transform.Find("heart1").gameObject.GetComponent<Animator>(),
+            transform.Find("heart2").gameObject.GetComponent<Animator>()
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetHeartDisplay(int health)
+    {
+        for (int i = 0; i < hearts.Count; i++)
+        {
+            hearts[i].SetBool("Active", i < health);
+        }
+    }
+}
