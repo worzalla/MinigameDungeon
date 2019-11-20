cf1f5a5710070d83596042b67331a45691da3db4
Jun Yu (Jimmy) Ma <Jimmy.ma3615@gmail.com>
2019-11-20 17:50:19 -0600

    DodgeBats Game

---
+++ Assets/_Project/Scripts/Minigames/DodgeBats/Spawner2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner2: MonoBehaviour
+{
+    public GameObject tracker;
+    Vector2 SpawnPoint;
+    public float spawnrate = 1f;
+    public float next = 0.0f;
+    float randY;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > next)
+        {
+            next = Time.time + spawnrate;
+            randY = Random.Range(-3.7f, 4f);
+            SpawnPoint = new Vector2(transform.position.x, randY);
+            GameObject go = Instantiate(tracker, SpawnPoint, Quaternion.identity);
+            go.transform.parent = transform;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Minigames/DodgeBats/Trackers.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trackers : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+    public float movementspeed = 80f;
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Player.GetInstance().transform;
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+
+
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition((Vector2)transform.position + (movement * movementspeed * Time.deltaTime) * 9/4);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.gameObject.tag);
+        //if (collision.gameObject.tag == "lava")
+        //{
+            //Minigame.SetSuccess(false);
+            //Minigame.FinishMinigame();
+          //  Destroy(this.gameObject);
+        //}
+        if (collision.gameObject.tag == "Player")
+        {
+            Minigame.SetSuccess(false);
+            Minigame.FinishMinigame();
+            //Minigame.SetSuccess(true);
+            //Minigame.FinishMinigame();
+            //Destroy(this.gameObject);
+        }
+
+    }
+}
